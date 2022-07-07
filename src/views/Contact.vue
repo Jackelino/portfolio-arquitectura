db3dd210f066e40b5b9a5647f4cdc5843a25263a
JoseFernandez2020 <codigo.gratuito2019@gmail.com>
2022-07-07 13:43:45 -0500

    se diseña la vista de Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,26 @@
 <template>
-  <div class="container mx-auto p-5">
-    contact
+  <div class="container">
+    <div class="container">
+      <h1 class="font-bold text-5xl pb-16 underline underline-offset-8">Contacto</h1>
+    </div>
+    <div class="container">
+      <div class="container py-6">
+        <div class="flex flex-row">
+          <div class="container self-center">
+            <ul class="">
+              <li class="pb-8 items-center flex hover:text-red-600"><i class="ti-location-pin text-3xl "></i><a href="" class="pl-6 ">198 West 21th Street, Suite 721 New York NY 10016</a></li>
+              <li class="pb-8 items-center flex hover:text-red-600"><i class="ti-mobile text-3xl"></i><a href="tel:1235 2355 980" class="pl-6">1235 2355 980</a></li>
+              <li class="pb-8 items-center flex hover:text-red-600"><i class="ti-email text-3xl"></i><a href="" class="pl-6">info@yoursite.com</a></li>
+            </ul>
+          </div>
+          <div class="container">
+            <figure>
+              <img src="../assets/img/interior-g6ccba1eb0_1280.jpg" alt="img">
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
